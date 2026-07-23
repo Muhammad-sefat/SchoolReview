c077a90f5052339d78de01bc2afd9ac1abb5150e
Setulal Chowhan <setulalchowhan@gmail.com>
2026-07-23 10:21:15 +0600

    feat: implement typographic components and configure Tailwind CSS theme with Urbanist font

--- src/components/typho/Title.jsx
+++ src/components/typho/Title.jsx
@@ -1,23 +1,23 @@
 export const Title48 = ({ children, className }) => {
-    return <p className={`${className} text-[48px]  font-bold leading-16 `}> {children} </p>
+    return <p className={`${className} text-[48px] font-urbanist  font-bold leading-16 `}> {children} </p>
 }
 
 export const Title24 = ({ children, className }) => {
-    return <p className={`${className} lg:text-[24px] text-lg  font-base leading-9 `}> {children} </p>
+    return <p className={`${className} lg:text-[24px] text-lg font-urbanist  font-base leading-9 `}> {children} </p>
 }
 
 export const Title32 = ({ children, className }) => {
-    return <p className={`${className} lg:text-[32px] text-[24px]  font-semibold leading-12 `}> {children} </p>
+    return <p className={`${className} lg:text-[32px] text-[24px] font-urbanist   font-semibold leading-12 `}> {children} </p>
 }
 
 export const Title18 = ({ children, className }) => {
-    return <p className={`${className} lg:text-[18px] text-base  font-medium leading-7 `}> {children} </p>
+    return <p className={`${className} lg:text-[18px] text-base font-urbanist   font-medium leading-7 `}> {children} </p>
 }
 
 export const Title16 = ({ children, className }) => {
-    return <p className={`${className} text-[16px]  font-normal leading-6 `}> {children} </p>
+    return <p className={`${className} text-[16px]  font-normal font-urbanist  leading-6 `}> {children} </p>
 }
 
 export const Title20 = ({ children, className }) => {
-    return <p className={`${className} lg:text-[20px] text-base  font-normal leading-6 `}> {children} </p>
+    return <p className={`${className} lg:text-[20px] text-base font-urbanist   font-normal leading-6 `}> {children} </p>
 }
